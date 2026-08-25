1cdfe2fdd98cd3a2412dedd0bcf52884dc87c97a
Inoussa Souwaiba <souwaibainoussa8@gmail.com>
2026-08-25 19:20:44 +0000

    mini to-do list : ajout de tâche avec input, bouton et liste dynamique

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ document.getElementById("titre").textContent="Salut souwaiba";
 document.getElementById("monboutton").addEventListener("click",function(){
     document.getElementById("titre").textContent="le bouton a été cliqué";
 });
+
+/*document.getElementById("boutonAjouter").addEventListener("click",function(){
+    let texte = document.getElementById("champtexte").value;
+    document.getElementById("liste").innerHTML+="<li>" + texte + "</li>";
+});*/
+
+document.getElementById("boutonAjouter").addEventListener("click", function() {
+  
+  // Étape 1 : récupérer le texte tapé
+  let texte = document.getElementById("champtexte").value;
+  
+  // Étape 2 : construire le HTML à ajouter
+  let nouvelElement = "<li>" + texte + "</li>";
+  
+  // Étape 3 : ajouter ce HTML à la liste
+  document.getElementById("liste").innerHTML += nouvelElement;
+  //Etape 4 : vider le champ après ajout
+  document.getElementById("champtexte").value="";
+});
