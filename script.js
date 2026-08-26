6c905aa08528ff6d6892c3a178ef7f90efee50d3
Inoussa Souwaiba <souwaibainoussa8@gmail.com>
2026-08-26 09:27:22 +0000

    Ajout compteur de tâches, touche Entrée et style css complet

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let titre = document.getElementById("titre");
+/*let titre = document.getElementById("titre");
 document.body.innerHTML += "<p> ça marche!</p>";
-document.getElementById("titre").textContent="Salut souwaiba";
+document.getElementById("titre").textContent="Salut souwaiba";*/
 /*document.getElementById("monboutton").addEventListener("click",function(){
     alert("que du succès insha ALLAH");
 });*/
 
-document.getElementById("monboutton").addEventListener("click",function(){
+/*document.getElementById("monboutton").addEventListener("click",function(){
     document.getElementById("titre").textContent="le bouton a été cliqué";
-});
+});*/
 
 /*document.getElementById("boutonAjouter").addEventListener("click",function(){
     let texte = document.getElementById("champtexte").value;
     document.getElementById("liste").innerHTML+="<li>" + texte + "</li>";
 });*/
 
-document.getElementById("boutonAjouter").addEventListener("click", function() {
+/*document.getElementById("boutonAjouter").addEventListener("click", function() {
   
   // Étape 1 : récupérer le texte tapé
   let texte = document.getElementById("champtexte").value;
@@ -27,4 +27,45 @@ document.getElementById("boutonAjouter").addEventListener("click", function() {
   //Etape 4 : vider le champ après ajout
   document.getElementById("champtexte").value="";
   }
+}); 
+document.getElementById("champtexte").addEventListener("keydown",function(event){
+    
+        let texte=document.getElementById("champtexte").value;
+    if(event.key=="Enter"){
+        let nouvelElement=  "<li>" + texte + " <button onclick='this.parentElement.remove()'>Supprimer</button></li>";  
+        document.getElementById("liste").innerHTML += nouvelElement;
+        document.getElementById("champtexte").value="";
+    }
+});*/
+function Ajouterlatache(){
+let texte=document.getElementById("champtexte").value;
+if(texte !== ""){
+let nouvelElement="<li>" + texte + "<button onclick='this.parentElement.remove(); mettreajoutcompteur();'>Supprimer</button></li>";
+document.getElementById("liste").innerHTML+=nouvelElement;
+document.getElementById("champtexte").value=""; 
+mettreajoutcompteur();
+}
+}
+
+document.getElementById("boutonAjouter").addEventListener("click",function(){
+    Ajouterlatache();
+})
+document.getElementById("champtexte").addEventListener("keydown",function(event){
+    if(event.key === "Enter"){
+Ajouterlatache();
+    }
+    
 });
+function mettreajoutcompteur(){
+let nombre = document.getElementById("liste").children.length;
+document.getElementById("compteur").textContent="tu as"  + nombre + "tâche(s) a faire";
+
+ }
+
+
+
+
+
+
+
+
